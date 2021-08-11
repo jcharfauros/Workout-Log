b1366701d0b6bf18f80cc2f2d1c7b899cba35fbd
jcharfauros <julia.charfauros@gmail.com>
2021-08-10 20:51:26 -0400

    Delete unused code

--- server/Readme.md
+++ server/Readme.md
@@ -4,21 +4,34 @@ Screenshots for Workout Server assignment. Server side is fully functional.
 
 ## Register a user
 
-![image info](assets/WL_register.jpg)
+![Postman - Register](https://github.com/jcharfauros/Workout-Log/blob/master/server/assets/WL_register.png)
+
+## Log in
+
+## Create a Workout log entry
+
+## Get
+
+Get all workout entries
+
+Get an individual workout entry
+
+## Update
+
+## Delete
+
+## Folders that were created
 
 - Middleware
 - Controllers
 - Models
 
-## Create files
+## Created files
 
 - app.js
 - db.js
 - .env
 - .gitignore
-
-## Build As needed
-
 - user controller
 - log controller
 - validate session
--- server/controllers/logcontroller.js
+++ server/controllers/logcontroller.js
@@ -3,19 +3,16 @@ const router = express.Router();
 const validateSession = require('../middleware/validate-session');
 const Log = require('../db').import('../models/log');
 
-// router.get('/practice', validateSession, function(req, res) 
-// {
-//     res.send('hey this is a test of the log route')
-// })
-
 /* LOG CREATE - POST*/
 router.post('/create', validateSession, (req, res) => {
+    
     const workoutLogEntry = {
         description: req.body.log.description,
         definition: req.body.log.definition,
         result: req.body.log.result,
         owner_id: req.user.id
     }
+
     Log.create(workoutLogEntry)
         .then(log => res.status(200).json(log))
         .catch(err => res.status(500).json({ error: err }))
@@ -23,6 +20,7 @@ router.post('/create', validateSession, (req, res) => {
 
 /* GET All - GET */
 router.get('/', (req, res) => {
+
     Log.findAll()
         .then(logs => res.status(200).json(logs))
         .catch(err => res.status(500).json({ error: err }))
